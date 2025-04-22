cdb18ba57b2ba5e86bd859d6f2a1bf7c59307296
jlescarlan11 <jlescarlan11@gmail.com>
2025-04-22 09:36:02 +0800

    added confirm password anf some validation

--- controllers/clubHouseController.js
+++ controllers/clubHouseController.js
@@ -1,6 +1,63 @@
 const passport = require("passport");
 const db = require("../db/queries");
 const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+
+const messageTrainerErr = "must be between 1 and 50 characters.";
+
+const { body } = require("express-validator");
+
+const validationSignUp = [
+  body("first_name")
+    .trim()
+    .notEmpty()
+    .withMessage("First name is required")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("First name must be between 2-50 characters")
+    .matches(/^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/)
+    .withMessage(
+      "First name can only contain letters, accents, spaces, apostrophes, and hyphens"
+    ),
+
+  body("last_name")
+    .trim()
+    .notEmpty()
+    .withMessage("Last name is required")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Last name must be between 2-50 characters")
+    .matches(/^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/)
+    .withMessage(
+      "Last name can only contain letters, accents, spaces, apostrophes, and hyphens"
+    ),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email format")
+    .normalizeEmail(),
+
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    )
+    .withMessage(
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    ),
+
+  body("confirmPassword")
+    .trim()
+    .notEmpty()
+    .withMessage("Confirm Password is required")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Passwords do not match"),
+];
 
 exports.clubHouseGet = (req, res, next) => {
   console.log(req.user);
@@ -15,16 +72,19 @@ exports.signUpGet = (req, res, next) => {
   res.render("sign-up-form");
 };
 
-exports.signUpPost = async (req, res, next) => {
-  try {
-    const { first_name, last_name, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await db.signUpUser(first_name, last_name, email, hashedPassword);
-    res.redirect("/");
-  } catch (err) {
-    next(err);
-  }
-};
+exports.signUpPost = [
+  validationSignUp,
+  async (req, res, next) => {
+    try {
+      const { first_name, last_name, email, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await db.signUpUser(first_name, last_name, email, hashedPassword);
+      res.redirect("/");
+    } catch (err) {
+      next(err);
+    }
+  },
+];
 
 exports.logInGet = async (req, res, next) => {
   res.render("log-in-form");
